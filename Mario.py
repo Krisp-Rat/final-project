e0fe75c710bfecc6b1a34072ff6e0b4d95d5fea1
awseq12 <angustsang20036125@gmail.com>
2024-11-05 13:43:51 -0500

    check this one

--- Mario.py
+++ Mario.py
@@ -1,7 +1,9 @@
-# make sure to use python 3.8
+# make sure to use python 3.8, use virtual enviornment and reopen vscode 
 from nes_py.wrappers import JoypadSpace
 import gym_super_mario_bros # install the VS code Developer kit?? refer to nes_py
 from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
 import gym
 import retro
+import pickle
+
 
