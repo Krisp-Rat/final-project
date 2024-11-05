7eb66208695127cf262eb8fac0ae87c61e1224a2
awseq12 <angustsang20036125@gmail.com>
2024-11-05 13:40:18 -0500

    got them to work? idk how

---
+++ Mario.py
@@ -0,0 +1,7 @@
+# make sure to use python 3.8
+from nes_py.wrappers import JoypadSpace
+import gym_super_mario_bros # install the VS code Developer kit?? refer to nes_py
+from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
+import gym
+import retro
+
